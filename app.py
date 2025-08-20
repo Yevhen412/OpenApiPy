b30489566b3dbdd30998fb2e7fede833771b945b
Yevhen412 <neogen123456789@gmail.com>
2025-08-21 00:11:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ async def stream_quotes():
 # Точка входа при запуске напрямую (не обязательна на Railway)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
